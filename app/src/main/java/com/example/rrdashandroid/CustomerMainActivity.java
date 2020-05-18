a3baef2c6592538d1f3d47b935e23537888aecb5
Robert Rahardja <robertrahardja@fb.com>
2020-05-18 16:39:35 +0800

    added second activity and linked them

--- app/src/main/java/com/example/rrdashandroid/CustomerMainActivity.java
+++ app/src/main/java/com/example/rrdashandroid/CustomerMainActivity.java
@@ -1,8 +1,8 @@
 package com.example.rrdashandroid;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class CustomerMainActivity extends AppCompatActivity {
 
--- app/src/main/java/com/example/rrdashandroid/SignInActivity.java
+++ app/src/main/java/com/example/rrdashandroid/SignInActivity.java
@@ -1,7 +1,9 @@
 package com.example.rrdashandroid;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,6 +14,13 @@ public class SignInActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
 
-        Log.d("My debug", "My message");
+        Button buttonLogin = (Button) findViewById(R.id.button_login);
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), CustomerMainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
