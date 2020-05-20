886566e237547589d16e75791840b7e2ae408d1e
Robert Rahardja <robertrahardja@fb.com>
2020-05-20 13:09:57 +0800

    Added Driver Screen

---
+++ app/src/main/java/com/example/rrdashandroid/DriverMainActivity.java
@@ -0,0 +1,78 @@
+package com.example.rrdashandroid;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class DriverMainActivity extends AppCompatActivity {
+
+//    Toolbar toolbar = findViewById(R.id.toolbar);
+//    setSupportActionBar(toolbar);
+
+    private DrawerLayout mDrawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_main);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
+
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.icon_menu_24dp);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+
+        mDrawerLayout = findViewById(R.id.drawer_layout_driver);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+
+                        // close drawer when item is tapped
+                        mDrawerLayout.closeDrawers();
+
+                        // Add code here to update the UI based on the item selected
+                        // For example, swap UI fragments here
+
+                        int id = menuItem.getItemId();
+                        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+                        if (id == R.id.nav_orders) {
+//                            transaction.replace(R.id.content_frame, new RestaurantListFragment()).commit();
+                        } else if (id == R.id.nav_delivery) {
+//                            transaction.replace(R.id.content_frame, new TrayFragment()).commit();
+                        } else if (id == R.id.nav_statistic) {
+//                            transaction.replace(R.id.content_frame, new OrderFragment()).commit();
+                        } else if (id == R.id.nav_logout) {
+                        }
+
+                        return true;
+                    }
+                });
+//        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+//        transaction.replace(R.id.content_frame, new RestaurantListFragment()).commit();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
--- app/src/main/java/com/example/rrdashandroid/SignInActivity.java
+++ app/src/main/java/com/example/rrdashandroid/SignInActivity.java
@@ -1,6 +1,7 @@
 package com.example.rrdashandroid;
 
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,17 +10,52 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class SignInActivity extends AppCompatActivity {
 
+    Button customerButton, driverButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
 
+        customerButton = (Button)findViewById(R.id.button_customer);
+        driverButton = (Button)findViewById(R.id.button_driver);
+
+        customerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                customerButton.setBackgroundColor(getResources().getColor(R.color.colorAccent));
+                customerButton.setTextColor(getResources().getColor(android.R.color.white));
+
+                driverButton.setBackgroundColor(getResources().getColor(android.R.color.white));
+                driverButton.setTextColor(getResources().getColor(R.color.colorAccent));
+            }
+        });
+
+        driverButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                driverButton.setBackgroundColor(getResources().getColor(R.color.colorAccent));
+                driverButton.setTextColor(getResources().getColor(android.R.color.white));
+
+                customerButton.setBackgroundColor(getResources().getColor(android.R.color.white));
+                customerButton.setTextColor(getResources().getColor(R.color.colorAccent));
+            }
+        });
+
         Button buttonLogin = (Button) findViewById(R.id.button_login);
         buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), CustomerMainActivity.class);
-                startActivity(intent);
+            public void onClick(View view){
+                ColorDrawable customerButtonColor = (ColorDrawable) customerButton.getBackground();
+
+                if (customerButtonColor.getColor() == getResources().getColor(R.color.colorAccent)){
+                    Intent intent = new Intent(getApplicationContext(), CustomerMainActivity.class);
+                    startActivity(intent);
+                }else {
+                    Intent intent = new Intent(getApplicationContext(), DriverMainActivity.class);
+                    startActivity(intent);
+                }
+
             }
         });
     }
